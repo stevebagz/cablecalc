82d3b270f9865df6ffd2d4481650b920900074c2
stevebagz <drathok@gmail.com>
2024-05-10 22:14:08 -0300

    update conversion rates, add color, decimal rounding, disclaimer

--- script.js
+++ script.js
@@ -2,31 +2,29 @@
 function calculate() {
     let wireSelection = document.getElementById("wireSize").value;
     let cableLength = document.getElementById("wireLength").value;
-    let rg6LowLossPer100 = 2.83;
-    let rg6HighLossPer100 = 7.31;
-    let rg11LowLossPer100 = 1.49;
-    let rg11HighLossPer100 = 3.85;
+    let rg6LowLossPer100 = 2.3;
+    let rg6HighLossPer100 = 5.65;
+    let rg11LowLossPer100 = 1.35;
+    let rg11HighLossPer100 = 3.65;
     let answerLow = null;
     let answerHigh = null;
     let inputLow = document.getElementById("lowFreq").value;
     let inputHigh = document.getElementById("hiFreq").value;
 
     if (wireSelection === "RG6") {
-        answerLow = ((cableLength/100) * rg6LowLossPer100);
-        answerHigh = ((cableLength/100) * rg6HighLossPer100);
+        answerLow = Math.round(((cableLength/100) * rg6LowLossPer100) * 10) /10;
+        answerHigh = Math.round(((cableLength/100) * rg6HighLossPer100) * 10) /10;
         
-        document.getElementById("answerText").innerText = `Loss on Ch.98 (111MHz) - ${answerLow}
-        Loss on Ch.110 (741MHz) - ${answerHigh}
-        Ch. 98 (111MHz) Level at tap is ${inputLow}, level at end of drop should be ${inputLow - answerLow}
-        Ch. 110 (741MHz) Level at tap is ${inputHigh}, level at end of drop should be ${inputHigh - answerHigh}`
+        document.getElementById("answerText").innerText = `Ch. 98 / Ch. 110 should read approx. ${inputLow - answerLow} / ${inputHigh - answerHigh} 
+        Loss on Ch.98 (111MHz) - ${answerLow} dBmV
+        Loss on Ch.110 (741MHz) - ${answerHigh} dBmV`
     }
     else {
-        answerLow = ((cableLength/100) * rg11LowLossPer100);
-        answerHigh = ((cableLength/100) * rg11HighLossPer100);
+        answerLow = Math.round(((cableLength/100) * rg11LowLossPer100) * 10) /10;
+        answerHigh = Math.round(((cableLength/100) * rg11HighLossPer100) * 10) /10;
         
-        document.getElementById("answerText").innerText = `Loss on Ch.98 (111MHz) - ${answerLow}
-        Loss on Ch.110 (741MHz) - ${answerHigh}
-        Ch. 98 (111MHz) Level at tap is ${inputLow}, level at end of drop should be ${inputLow - answerLow}
-        Ch. 110 (741MHz) Level at tap is ${inputHigh}, level at end of drop should be ${inputHigh - answerHigh}`
+        document.getElementById("answerText").innerText = `Ch. 98 / Ch. 110 should read approx. ${inputLow - answerLow} / ${inputHigh - answerHigh} 
+        Loss on Ch.98 (111MHz) - ${answerLow} dBmV
+        Loss on Ch.110 (741MHz) - ${answerHigh} dBmV`
     }
 }
